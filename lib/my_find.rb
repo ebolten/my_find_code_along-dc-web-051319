06a682056830bd63f735fabddf2d87383c60728c
Emily Bolten <emilybolten@gmail.com>
2019-05-13 21:17:30 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,12 @@
 require 'pry'
 
 def my_find(collection)
-
+  i = 0
+  val = nil
+  while i < collection.length
+    return collection[i] if yield(collection[i])
+    i = i + 1
+    
+  end
+  return val
 end
