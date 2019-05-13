371748340c6aff40b3e4b3c7cd4c86be39dfdc98
Emily Bolten <emilybolten@gmail.com>
2019-05-13 21:16:32 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,12 +3,12 @@ require 'pry'
 def my_find(collection)
   i = 0
   val = nil
-  vals = []
   while i < collection.length
     i += 1
     if yield(collection[i - 1]) == true
-      return collection[i - 1]
+      val =  collection[i - 1]
       break
     end
   end
+  return val
 end
